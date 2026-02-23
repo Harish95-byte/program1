ded722d8c4583189f40fab50f412de64e5b3fee2
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-23 12:34:56 +0530

    first commit

---
+++ src/test/java/com/example/program1test.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class prgram1test{
+    program1 m=new program1()
+
+    @Test
+    void test1(){
+        assertEquals("This is Program 1 - Maven Jenkins Demo",m.main())
+    }
+
+}
+
